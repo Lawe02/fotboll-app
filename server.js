11a8ddea658ab54fa167f9768ce6388c4af6e603
Lawe02 <lawza02@gmail.com>
2022-05-27 20:06:23 +0200

    routes for data and index works

--- server.js
+++ server.js
@@ -2,12 +2,18 @@
 
 const { urlencoded } = require('body-parser');
 const express = require('express');
+const router = require ('./routes');
 const app = express();
 const port = 8020;
+const indexRouter = require('./routes/index');
+const dataRouter = require('./routes/data')
 
 app.use(express.urlencoded());
 
 app.use(express.static('public'));
+
+app.use('/', indexRouter);
+app.use('/data', dataRouter);
 
 app.listen(port);
 
