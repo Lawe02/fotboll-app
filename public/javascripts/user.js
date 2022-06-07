c984ca798c66bf512740901c35e85b486b82cc75
Lawe02 <lawza02@gmail.com>
2022-06-07 18:37:38 +0200

    working on fronted at admin

--- public/javascripts/user.js
+++ public/javascripts/user.js
@@ -17,12 +17,15 @@ myFunction()
 });
 
 function w3_open() {
+  document.getElementById("main").style.marginLeft = "30%";
+  document.getElementById("mySidebar").style.width = "30%";
   document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
 }
 function w3_close() {
+  document.getElementById("main").style.marginLeft = "0%";
   document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
 }
-
-
 
 
