360320796c48e80615542774672038c3fc330c09
Lawe02 <lawza02@gmail.com>
2022-06-10 01:00:31 +0200

    changed port variables so that heroku can assign one

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ require('body-parser');
 const express = require('express');
 require('./routes');
 const app = express();
-const port = 8020;
 const indexRouter = require('./routes/index');
 const dataRouter = require('./routes/data')
 
@@ -15,8 +14,8 @@ app.use(express.static('public'));
 app.use('/', indexRouter);
 app.use('/data', dataRouter);
 
-app.listen(port);
+app.listen(process.env.PORT || 5000);
 
-console.log('Server up and running, listning on port' + port);
+console.log('Server up and running, listning on port 5000');
 
 module.exports = app;
